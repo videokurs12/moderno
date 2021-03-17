2e38aba7eda3c85e202df1296256e78392309d50
videokurs12 <airincars@gmail.com>
2021-03-17 17:45:41 +0200

    added header

--- gulpfile.js
+++ gulpfile.js
@@ -9,8 +9,8 @@ let gulp = require('gulp'),
 
 
     gulp.task('sass', function(){
-      return gulp.src('app/scss/style.scss')
-            .pipe(sass({outputStyle: 'compressed'}))
+      return gulp.src('app/scss/**/*.scss')
+            .pipe(sass({outputStyle: 'expanded'}))
             .pipe(rename({suffix : '.min'}))
             .pipe(autoprefixer({
               overrideBrowserslist: ['last 8 versions']
@@ -59,7 +59,7 @@ let gulp = require('gulp'),
     });
 
     gulp.task('watch', function(){
-      gulp.watch('app/scss/style.scss', gulp.parallel('sass'));
+      gulp.watch('app/scss/**/*.scss', gulp.parallel('sass'));
       gulp.watch('app/*.html', gulp.parallel('html'));
       gulp.watch('app/js/*.js', gulp.parallel('js'));
     });
